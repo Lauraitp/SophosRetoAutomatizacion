fc459986c82ecca719fdf67de568060a8ccdd779
laura.tabares <laura.tabares@CPX-2D7YP00A4E5.dir.svc.accenture.com>
2020-03-26 23:28:17 -0500

    merge

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-# serenity-restAssured
-Base project to test Rest APIs using Serenity
-=======
 # SophosRetoAutomatizacion
->>>>>>> branch 'master' of https://github.com/Lauraitp/SophosRetoAutomatizacion.git
-=======
-# SophosRetoAutomatizacion
->>>>>>> refs/remotes/origin/master
---
+++ src/test/java/com/sophos/challenge/stepdefinitions/AddShoppingCarStepDefinitions.java
@@ -0,0 +1,39 @@
+package com.sophos.challenge.stepdefinitions;
+
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+
+import com.sophos.challenge.tasks.OpenBrowser;
+
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+public class AddShoppingCarStepDefinitions {
+
+	@Before
+    public void set_the_stage() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+
+	
+	@Given("I enter into vivanda page")
+	public void iEnterIntoVivandaPage() {
+		theActorCalled("Laura").attemptsTo(OpenBrowser.openningBrowser("https://www.vivanda.com.pe/"));
+	}
+
+	@When("I add a product (.*) into shopping car")
+	public void iAddAProductIntoShoppingCar(String product) {
+	   
+	}
+
+	@Then("I verify that the product selected is in the shopping car")
+	public void iVerifyThatTheProductSelectedIsInTheShoppingCar() {
+	    
+	}
+
+	
+}
