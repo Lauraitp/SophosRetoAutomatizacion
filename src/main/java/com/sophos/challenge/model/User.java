3e06c6fc6b1495a405eae17976a757e908cd6e02
laura.tabares <laura.tabares@CPX-2D7YP00A4E5.dir.svc.accenture.com>
2020-03-28 16:14:07 -0500

    Adicionar documentación

---
+++ src/main/java/com/sophos/challenge/model/User.java
@@ -0,0 +1,24 @@
+package com.sophos.challenge.model;
+
+public class User {
+	
+	/**
+	 * Objeto del usuario con el atributo de nombre
+	 */
+
+	private String name;
+	
+	
+	public User() {
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+}
